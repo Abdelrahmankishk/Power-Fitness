cf81ab8bbf59e107ae65d2975ca17bdcba61b0d2
Abdelrahmankishk <abdelrahmankishk@gmail.com>
2026-06-26 20:45:04 +0300

    add Plans View (index Action) and display all available plans from database

---
+++ GymManagementSystem/Controllers/PlansController.cs
@@ -0,0 +1,21 @@
+﻿using GymManagementSystem.DbContexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace GymManagementSystem.Controllers
+{
+    public class PlansController : Controller
+    {
+        private readonly GymDbContext dbContext;
+        public PlansController()
+        {
+            dbContext = new GymDbContext();
+        }
+        public async Task<IActionResult> Index()
+        {
+            var plans = await dbContext.plans.ToListAsync();
+            return View(plans);
+        }
+    }
+}
