eac85efb38ea97dcb599f332d8db93a34c6ae2ff
Abdelrahmankishk <abdelrahmankishk@gmail.com>
2026-06-26 17:17:05 +0300

    add Plan Entity Class & DbContext for initial Connection with the Database

---
+++ GymManagementSystem/DbContexts/GymDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GymManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagementSystem.DbContexts
+{
+    public class GymDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=.;Database=PowerFitnessdb;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+        public DbSet<Plan> plans { get; set; }
+    }
+}
